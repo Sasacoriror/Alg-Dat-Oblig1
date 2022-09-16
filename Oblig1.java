a211fb7cab56be15a4d5904d161917fdee4b8906
Sasacoriror <43811709+Sasacoriror@users.noreply.github.com>
2022-09-16 15:15:41 +0200

    Add files via upload

--- Oblig1.java
+++ Oblig1.java
@@ -1,6 +1,7 @@
 ////// Løsningsforslag Oblig 1 ////////////////////////
 
 import java.lang.UnsupportedOperationException;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class Oblig1 {
@@ -78,55 +79,90 @@ public class Oblig1 {
 
     ///// Oppgave 4 //////////////////////////////////////
     public static void delsortering(int[] a) {
-        throw new UnsupportedOperationException();
+        int partall = 0;
+        int oddetall = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0){
+                partall++;
+            }else{
+                oddetall++;
+            }
+        }
+
+        int[] innPartall = new int[partall];
+        int[] innOdetall = new int[oddetall];
+
+        int partallIndex = 0;
+        int oddetallIndex = 0;
+
+        for (int i = 0; i < a.length; i++){
+            if (a[i] % 2 == 0){
+                innPartall[partallIndex] = a[i];
+                partallIndex++;
+            }else {
+                innOdetall[oddetallIndex] = a[i];
+                oddetallIndex++;
+            }
+        }
+
+        Arrays.sort(innPartall);
+        Arrays.sort(innOdetall);
+
+        for (int i = 0; i < innOdetall.length; i++){
+            a[i] = innOdetall[i];
+        }
+
+        for (int i = 0; i < innPartall.length; i++){
+            a[oddetall] = innPartall[i];
+            oddetall++;
+        }
     }
 
     ///// Oppgave 5 //////////////////////////////////////
     public static void rotasjon(char[] a) {
-        // Flytter alle verdiene i arrayet en plass til høyre
         if (a.length > 1) {
-            // Initierer en hjelpe variabel med verdien til den siste bokstaven i tabellen
             char tmp = a[a.length - 1];
-            // Kopierer arrayet og flytter det en plass til høyre ved hjelp av arraycopy
             System.arraycopy(a, 0, a, 1, a.length - 1);
-            // Plasserer hjelpevariablen på plass a[0]
             a[0] = tmp;
         }
-    }
-
-    ///// Oppgave 6 //////////////////////////////////////
-    public static void rotasjon(char[] a, int k) {
-        throw new UnsupportedOperationException();
     }
 
     ///// Oppgave 7 //////////////////////////////////////
     /// 7a)
     public static String flett(String s, String t) {
-        throw new UnsupportedOperationException();
+        StringBuilder flettetString = new StringBuilder();
+
+        for(int i = 0; i < s.length() || i < t.length(); ++i) {
+            if(i < s.length()) {
+                flettetString.append(s.charAt(i));
+            }
+            if(i < t.length()) {
+                flettetString.append(t.charAt(i));
+            }
+        }
+        return flettetString.toString();
     }
 
     /// 7b)
     public static String flett(String... s) {
-        throw new UnsupportedOperationException();
-    }
+        StringBuilder flettetString = new StringBuilder();
+        int charIndex = 0;
+        boolean ferdig = false;
 
-    ///// Oppgave 8 //////////////////////////////////////
-    public static int[] indekssortering(int[] a) {
-        throw new UnsupportedOperationException();
-    }
+        while (!ferdig) {
+            ferdig = true;
 
-    ///// Oppgave 9 //////////////////////////////////////
-    public static int[] tredjeMin(int[] a) {
-        throw new UnsupportedOperationException();
-    }
-
-    ///// Oppgave 10 //////////////////////////////////////
-    public static int bokstavNr(char bokstav) {
-        throw new UnsupportedOperationException();
-    }
-
-    public static boolean inneholdt(String a, String b) {
-        throw new UnsupportedOperationException();
+            for (String innhold : s) {
+                int lengde = innhold.length();
+                if (lengde > charIndex) {
+                    flettetString.append(innhold.charAt(charIndex));
+                    ferdig = false;
+                }
+            }
+            charIndex++;
+        }
+        return flettetString.toString();
     }
 
 }  // Oblig1
